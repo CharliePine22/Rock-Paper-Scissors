1485dfd93f66f5a3e563625043c36354ad1a337d
Charles Pine <charlespine22@gmail.com>
2020-07-29 15:50:23 -0500

    Added in a while loop to incorporate rounds. Working on score system as well.

--- game.js
+++ game.js
@@ -8,38 +8,44 @@ function computerPlay() {
     }
     else {
         return 'scissors'
+
     }
 } 
 
 let computerScore = 0;
 let playerScore = 0;
-let round = 0
-  
     function playGame(playerChoice, computerChoice) {
+    let round = 0
+    while(round < 6) {
     var computerChoice = computerPlay();
     var playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
-
     if(playerChoice === computerChoice) {
-        return 'Tie!';
+      console.log('Tie!');
+      round++;
     }
     else if(playerChoice == 'rock' && computerChoice == 'scissors') {
         playerScore++;
+        console.log(`Player chose rock and computer chose scissors! The player wins! Player: ${playerScore} Computer:${computerScore}`);
         round++;
-        return `Player chose rock and computer chose scissors! The player wins! Player: ${playerScore} Computer:${computerScore}`;
     }
     else if(playerChoice == 'paper' && computerChoice == 'rock') {
         playerScore++;
-        return `Player chose paper and computer chose rock! The player wins! Player: ${playerScore} Computer:${computerScore}`;
+        round++;
+        console.log(`Player chose paper and computer chose rock! The player wins! Player: ${playerScore} Computer:${computerScore}`);
     }
     else if(playerChoice == 'scissors' && computerChoice == 'paper') {
         playerScore++;
-        return `Player chose scissors and computer chose paper! The player wins! Player: ${playerScore} Computer:${computerScore}`;
+        round++;
+        console.log(`Player chose scissors and computer chose paper! The player wins! Player: ${playerScore} Computer:${computerScore}`);
     }
     else {
         computerScore++;
-        return `The player chose ${playerChoice} and the computer chose ${computerChoice}! The computer wins! Player: ${playerScore} Computer: ${computerScore}.`
+        console.log(`The player chose ${playerChoice} and the computer chose ${computerChoice}! The computer wins! Player: ${playerScore} Computer: ${computerScore}.`)
     }
+  }
   }
 
 
+computerPlay()
+console.log(playGame('rock'))
 
